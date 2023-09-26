2fca2af5e8d790618bcce7b5f55219f864a5aa39
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2023-09-26 12:31:33 +0300

    uploaded lab3 first task

--- LAB3/crawler.py
+++ LAB3/crawler.py
@@ -2,41 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import re
 # Replace with the URL of the web page you want to scrape
-url = "https://999.md/ro/list/transport/bicycles"
+url = "https://999.md/ro/list/transport/bicycles?page=1"
 
-
-reqs = requests.get(url)
-soup = BeautifulSoup(reqs.text, 'html.parser')
- 
-urls = []
-for link in soup.find_all('a'):
-    print(link.get('href'))
-
-
-'''
 def scrape(site, urls, depth):
+    print("page " + site[-1])
+    reqs = requests.get(site)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
     
-    # getting the request from url
-    r = requests.get(site)
-       
-    # converting the text
-    s = BeautifulSoup(r.text,"html.parser")
-       
-    for i in s.find_all("a"):         
-        href = i.attrs['href']
-        
-        
-               
-        if depth > 0:
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if re.search(r"/ro/[0-9][0-9]", str(href)) and href not in urls:
             urls.append(href)
-            if href.startswith("https://999.md/ro/"):
-                urls.append(href)   
-                print(href)              
-            # calling it self
-            scrape(site, urls, depth-1)
-        else:
-            return urls
+    
+    if int(site[-1]) <= depth:
+        scrape("https://999.md/ro/list/transport/bicycles?page=" + str(int(site[-1]) + 1), urls, depth)
+    return urls
 
+urls = []
+scrape(url, urls, 3)
+for lnk in urls:
+    print("https://999.md" + lnk)
 
-print(scrape(url, urls, 5))
-'''
